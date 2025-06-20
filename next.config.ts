3e417098fabee50ebaac4ec9890787867650b7f7
Hussein Othman <127331707+hussein1997323@users.noreply.github.com>
2025-06-20 15:22:22 -0300

    w

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: ["yt3.googleusercontent.com"], // Adicione aqui os domínios permitidos
   },
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { CiGrid32, CiBank } from "react-icons/ci";
