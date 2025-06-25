80d756cd14dfa534b3229df14b78a820bf7f84f5
Hussein Othman <127331707+hussein1997323@users.noreply.github.com>
2025-06-25 15:09:27 -0300

    sss

--- src/services/services.tsx
+++ src/services/services.tsx
@@ -3,5 +3,22 @@ import axios from "axios";
 const makeRequest = axios.create({
   baseURL: "",
 });
+makeRequest.interceptors.request.use(
+  (config) => {
+    if (typeof window !== "undefined") {
+      const tokenData = localStorage.getItem("sistema-cadastro-keite:token");
+      if (tokenData) {
+        const { token } = JSON.parse(tokenData);
+        if (token && config.headers) {
+          config.headers["Authorization"] = `Bearer ${token}`;
+        }
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default makeRequest;
