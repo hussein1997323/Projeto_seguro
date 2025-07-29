96946ea82d9334a35e63908c672d13c3970de8dd
Hussein Othman <127331707+hussein1997323@users.noreply.github.com>
2025-07-29 15:26:33 -0300

    ss

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ const nextConfig = {
   output: "export", // 👈 Adiciona essa linha para permitir exportação estática
   trailingSlash: true, // 👈 Adiciona isso para garantir que os links funcionem corretamente em hospedagens simples
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://husseindev.com.br/api/:path*",
-      },
-    ];
-  },
-
   images: {
     domains: ["yt3.googleusercontent.com"],
   },
