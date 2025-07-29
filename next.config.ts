8519a9404f3054b14903f26a1174e94a7d3e8b2d
Hussein Othman <127331707+hussein1997323@users.noreply.github.com>
2025-07-29 20:52:30 -0300

    finss

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // 👈 Adiciona essa linha para permitir exportação estática
-  trailingSlash: true, // 👈 Adiciona isso para garantir que os links funcionem corretamente em hospedagens simples
+  trailingSlash: true,
+  devIndicators: false, // 👈 Adiciona isso para garantir que os links funcionem corretamente em hospedagens simples
 
   images: {
     domains: ["yt3.googleusercontent.com"],
